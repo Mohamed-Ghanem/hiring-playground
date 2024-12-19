5d77a0ee2151ec666730595d0d112792615c731c
Mohamed Ghanem <ma-ghanem@hotmail.com>
2024-12-19 12:43:57 +0200

    Update service functions to use mockups service

--- frontend/modules/services/products.ts
+++ frontend/modules/services/products.ts
@@ -1,10 +1,14 @@
-import { rawData } from "./RawData";
+// import { rawData } from './RawData';
 import { CartProps, ProductProps } from './types';
 
 export const getProducts: () => Promise<ProductProps[]> = async () => {
-    return rawData.products as ProductProps[]
-}
+  // return rawData.products as ProductProps[]
+  return fetch('http://localhost:8082/products').then((data) => data.json());
+};
 
 export const getCart: () => Promise<CartProps[]> = async () => {
-    return rawData.carts[0].items as CartProps[]
-}
+  //   return rawData.carts[0].items as CartProps[];
+  return fetch('http://localhost:8082/carts')
+    .then((data) => data.json())
+    .then((data) => data[0].items);
+};
